4202ec6ada8e610c1c52691172782a8136ce6d8e
zabavb <bilonizkavik@gmail.com>
2025-07-22 13:47:16 +0200

    Implemented controller, service layers for performing CRUD operations along with setting rating to the recipe.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
+import recipeRoutes from './routes/recipe.routes';
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/recipes', recipeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
