818ea9a78fe9795ca3e527f01923edb92b674ebb
zabavb <bilonizkavik@gmail.com>
2025-07-22 14:35:16 +0200

    Created React app by default TypeScript template and configured tailwindcss.

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-indigo-600">Welcome to FlavorAI!</h1>
+    </div>
+  );
+}
+
+export default App;
